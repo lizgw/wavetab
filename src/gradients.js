1b1db9ec9ea4d22e5da3aff1af5ac162335aebbd
Liz Wigglesworth <liz.wigglesworth@gmail.com>
2024-06-09 12:04:38 -0500

    Update chrome API call

--- src/gradients.js
+++ src/gradients.js
@@ -4,7 +4,7 @@ var gradientData;
 function readFile()
 {
     // get the path of the json file(s)
-    var path = chrome.extension.getURL("assets/data/default.json");
+    var path = chrome.runtime.getURL("assets/data/default.json");
 
     // request and read the file
     var req = new XMLHttpRequest();
